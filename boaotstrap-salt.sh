215f16f4598d2577b78d3e982225a5ee8e9f87cf
granular-ryanbonham <41303664+granular-ryanbonham@users.noreply.github.com>
2024-12-18 13:11:57 -0500

    Create boaotstrap-salt.sh
    Initial linux support

---
+++ boaotstrap-salt.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#         NAME:  __fetch_url
+#  DESCRIPTION:  Retrieves a URL and writes it to a given path
+#----------------------------------------------------------------------------------------------------------------------
+__fetch_url() {
+
+    # shellcheck disable=SC2086
+    curl $_CURL_ARGS -L -s -f -o "$1" "$2" >/dev/null 2>&1     ||
+        wget $_WGET_ARGS -q -O "$1" "$2" >/dev/null 2>&1       ||
+            fetch $_FETCH_ARGS -q -o "$1" "$2" >/dev/null 2>&1 ||  # FreeBSD
+                fetch -q -o "$1" "$2" >/dev/null 2>&1          ||  # Pre FreeBSD 10
+                    ftp -o "$1" "$2" >/dev/null 2>&1           ||  # OpenBSD
+                        (echoerror "$2 failed to download to $1"; exit 1)
+}
+
+# Download the bootstrap-salt.sh script using __fetch_url
+__fetch_url "bootstrap-salt.sh" "https://github.com/saltstack/salt-bootstrap/releases/latest/download/bootstrap-salt.sh"
+
+# Run the downloaded script
+sh bootstrap-salt.sh
+
+# Check if the script was successful
+if [ $? -eq 0 ]; then
+    echo "Bootstrap script ran successfully."
+    # Run the salt-pip install command
+    echo "Installing Credstash."
+    salt-pip install credstash
+    echo "Corteva Bootstrap Complete."
+else
+    echo "Bootstrap script failed."
+    exit 1
+fi
