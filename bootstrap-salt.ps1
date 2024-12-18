55a037261a698b113100d6a01f864577a6762417
granular-ryanbonham <41303664+granular-ryanbonham@users.noreply.github.com>
2024-12-18 06:52:06 -0500

    Create bootstrap-salt.ps1
    Bootstrap salt with upstream script and then install credstash

---
+++ bootstrap-salt.ps1
@@ -0,0 +1,48 @@
+# Enable TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Define the URL of the script to download
+$scriptUrl = "https://github.com/saltstack/salt-bootstrap/releases/latest/download/bootstrap-salt.ps1"
+
+# Download the script
+$scriptContent = Invoke-WebRequest -Uri $scriptUrl -UseBasicParsing
+
+# Save the script to a temporary file
+$tempScriptPath = [System.IO.Path]::GetTempFileName() + ".ps1"
+Set-Content -Path $tempScriptPath -Value $scriptContent.Content
+
+# Execute the downloaded script with the same parameters
+& $tempScriptPath @args
+
+# Clean up the temporary file
+Remove-Item -Path $tempScriptPath
+
+# Check For Salt Pip and Innstall Credstash
+$path = "C:\Program Files\Salt Project\Salt\salt-pip.exe"
+$timeout = 300  # Timeout in seconds
+$interval = 5   # Interval between checks in seconds
+$elapsed = 0
+
+while (-not (Test-Path $path) -and ($elapsed -lt $timeout)) {
+    Write-Host "Waiting for $path to become valid..."  -ForegroundColor Yellow
+    Start-Sleep -Seconds $interval
+    $elapsed += $interval
+}
+
+if (Test-Path $path) {
+    Write-Host "$path is now valid."
+    # Proceed with your command
+    $saltPipInstall = Start-Process -FilePath $path -WorkingDirectory "C:\Program Files\Salt Project\Salt" -ArgumentList "install credstash" -NoNewWindow -Wait -PassThru
+    if ($saltPipInstall.ExitCode -ne 0) {
+        Write-Host "Credstash install failed with exit code $($saltPipInstall.ExitCode)" -ForegroundColor Red
+        exit 1
+    } else {
+        Write-Host "-------------------------------------------------------------------------------" -ForegroundColor Yellow
+        Write-Host "Credstash install succeeded" -ForegroundColor Green
+        Write-Host "===============================================================================" -ForegroundColor Yellow
+    }
+} else {
+    Write-Host "Timeout reached. $path is still not valid."  -ForegroundColor Red
+    exit 1
+}
+exit 0
